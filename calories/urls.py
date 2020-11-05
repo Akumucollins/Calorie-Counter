36da6251ff79048a6795f22a34260877278178d0
Akumucollins <akumucollins001@gmail.com>
2020-11-05 23:34:35 +0300

    added the views urlpattern for the api endpoint

---
+++ calories/urls.py
@@ -0,0 +1,15 @@
+from  django.urls import path, include
+from . import views 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('Profile', views.ProfileView)
+# router.register('Client', views.ClientView)
+router.register('Category', views.CategoryView)
+router.register('Fooditem', views.FooditemView)
+router.register('UserFooditem', views.UserFooditemView)
+
+
+urlpatterns=[
+path('', include(router.urls))
+]    
