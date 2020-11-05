c9a7b86c6f6ad4c6b067185f1ddca69cbb9b7f77
Akumucollins <akumucollins001@gmail.com>
2020-11-05 23:28:56 +0300

    created rest-framework view api endpoint function

--- calories/views.py
+++ calories/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import *
+from .serializers import *
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    
+class FooditemView(viewsets.ModelViewSet):
+    queryset = Fooditem.objects.all()
+    serializer_class = FooditemSerializer
+    
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class UserFooditemView(viewsets.ModelViewSet):
+    queryset = UserFooditem.objects.all()
+    serializer_class = UserFooditemSerializer                
